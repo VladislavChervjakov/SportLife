a7b680dcbf25b5ef94206e1fb9232aa05527dca4
Vlad <vlchervjakov@gmail.com>
2018-11-15 19:13:26 +0200

    added article page

--- controllers/ArticlesController.php
+++ controllers/ArticlesController.php
@@ -17,4 +17,8 @@ class ArticlesController extends Controller
     {
         return $this->render('index');
     }
+    public function  actionShow()
+    {
+        return $this->render('show');
+    }
 }
---
+++ views/articles/show.php
@@ -0,0 +1,51 @@
+<div class="content">
+    <div class="container">
+        <h3 class="text-center">Article Title</h3>
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12 article-image">
+                <img src="images/sport1.jpg" alt="">
+            </div>
+            <div class="col-md-12 col-lg-12 col-sm-12 article-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ab assumenda doloremque expedita laborum modi neque porro voluptatum!
+                Laudantium temporibus, totam?
+                Corporis doloribus eaque, fuga fugit hic inventore nulla quasi velit!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Assumenda fugiat, laboriosam minus quos rerum sit unde!
+                Autem cum dolores
+                fugiat laboriosam minus molestiae nisi pariatur praesentium,
+                rerum velit veritatis voluptatem!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ab assumenda doloremque expedita laborum modi neque porro voluptatum!
+                Laudantium temporibus, totam?
+                Corporis doloribus eaque, fuga fugit hic inventore nulla quasi velit!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Assumenda fugiat, laboriosam minus quos rerum sit unde!
+                Autem cum dolores
+                fugiat laboriosam minus molestiae nisi pariatur praesentium,
+                rerum velit veritatis voluptatem!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ab assumenda doloremque expedita laborum modi neque porro voluptatum!
+                Laudantium temporibus, totam?
+                Corporis doloribus eaque, fuga fugit hic inventore nulla quasi velit!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Assumenda fugiat, laboriosam minus quos rerum sit unde!
+                Autem cum dolores
+                fugiat laboriosam minus molestiae nisi pariatur praesentium,
+                rerum velit veritatis voluptatem!
+            </div>
+        </div>
+        <div class="comment-form text-center">
+            <div class="col-md-12">
+                <div class="panel ">
+                    <div class="panel-body">
+                        <textarea class="form-control" rows="2" placeholder="Добавьте Ваш комментарий"></textarea>
+                        <div class="mar-top clearfix">
+                            <button class="btn btn-sm btn-primary pull-right" type="submit"><i class="fa fa-pencil fa-fw"></i> Добавить</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
